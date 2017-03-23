bd784cadc66c5c193edd72d18fee75b8a27b73b1
solidkime <dr.kime@gmail.com>
2017-03-23 11:28:44 +0200

    addes some basic tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Question, type: :model do
   
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+  it { should belong_to :user }
 end
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe User do
 
   it {should validate_presence_of :email}
   it {should validate_presence_of :password}
+  it { should have_many(:questions).dependent(:destroy) }
+  it { should have_many(:answers).dependent(:destroy) }
 
   context "author_of?" do
     it 'should return true if author matches' do

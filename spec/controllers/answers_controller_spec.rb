d8bc18c74845e30f59143c3fa00ba9cc8041b538
solidkime <dr.kime@gmail.com>
2017-03-27 11:31:26 +0300

    more fixes

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -6,50 +6,7 @@ RSpec.describe AnswersController, type: :controller do
   let(:answer) { create :answer, question: question, user: user }
 
 
-  # describe "GET #index" do
-  #   sign_in_user
-
-  #   let(:answers) { create_list(:answer, 2, question: question, user: user) }
-  #   before do
-  #     get :index, params: { question_id: question }
-  #   end
-
-  #   it 'populates an array of all answers' do
-  #     expect(assigns(:answers)).to match_array(answers)
-  #   end
-
-  #   it 'renders index view' do
-  #     expect(response).to render_template :index
-  #   end
-  # end
-
-  # describe "GET #show" do
-  #   sign_in_user
-
-  #   before { get :show, params: { id: answer, question_id: question } }
-
-  #   it 'assigns the requested question to @answer' do
-  #     expect(assigns(:answer)).to eq answer
-  #   end
-
-  #   it 'renders show view' do
-  #     expect(response).to render_template :show
-  #   end
-  # end
-
-  # describe "GET #new" do
-  #   sign_in_user
-    
-  #   before { get :new, params: { id: answer, question_id: question } }
-  #   it 'assigns a new Answer to @answer' do
-  #     expect(assigns(:answer)).to be_a_new(Answer)
-  #   end
-
-  #   it 'renders new view' do
-  #     expect(response).to render_template :new
-  #   end
-  # end
-
+ 
   describe 'POST #create' do
     sign_in_user
 
@@ -64,10 +21,7 @@ RSpec.describe AnswersController, type: :controller do
         post :create, params: { answer: attributes_for(:answer), question_id: question }
         expect(response).to redirect_to question
       end
-      # it 'redirects to show view' do
-      #   post :create, params: { answer: attributes_for(:answer), question_id: question }
-      #   expect(response).to redirect_to question_answer_path(question, assigns(:answer))
-      # end
+      
     end
     context 'with invalid attributes' do
       it 'does not save the answer' do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,14 +11,28 @@ RSpec.describe User do
   it { should have_many(:questions).dependent(:destroy) }
   it { should have_many(:answers).dependent(:destroy) }
 
-  context "author_of?" do
-    it 'should return true if author matches' do
-      expect(user.author_of?(question)).to eq(true)
-      expect(user.author_of?(answer)).to eq(true)
+#   context "author_of?" do
+#     it 'should return true if author matches' do
+#       expect(user.author_of?(question)).to eq(true)
+#       expect(user.author_of?(answer)).to eq(true)
+#     end
+#     it 'should return false if author not matches' do
+#       expect(user2.author_of?(question)).to eq(false)
+#       expect(user2.author_of?(answer)).to eq(false)
+#     end
+#   end
+# end
+
+  describe "#author_of?" do
+    let(:user) { build_stubbed :user }
+    let(:target) { double }
+
+    it "returns true when author" do
+      allow(target).to receive(:user_id).and_return user.id
+      expect(user.author_of?(target)).to eq true
     end
-    it 'should return false if author not matches' do
-      expect(user2.author_of?(question)).to eq(false)
-      expect(user2.author_of?(answer)).to eq(false)
-    end
+    
   end
+
+  
 end

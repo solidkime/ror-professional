d809825be1db56d1b91c77fa0b919cc475467f30
solidkime <dr.kime@gmail.com>
2017-04-28 11:08:14 +0300

    added answer method mark_best

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,9 @@ class Answer < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
+
+  def make_best
+    question.answers.where("best = ?", true).update_all(best: false)
+    update!(best: true)
+  end
 end
